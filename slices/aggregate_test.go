5eb762ca7cf25e4f11fbde55ef6f6ae4d6fde5ea
windflowed <3458873494@qq.com>
2025-07-07 00:16:26 +0800

    add define of test aggregate

---
+++ slices/aggregate_test.go
@@ -0,0 +1,23 @@
+package slices
+
+import "testing"
+
+func TestMax(t *testing.T) {
+
+}
+
+func TestMin(t *testing.T) {
+
+}
+
+func TestSum(t *testing.T) {
+
+}
+
+func TestAvg(t *testing.T) {
+
+}
+
+func TestCount(t *testing.T) {
+
+}
