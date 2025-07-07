8054b17a8ca134cc90e00d532bdbd9e3bcfb9e4b
windflowed <3458873494@qq.com>
2025-07-07 10:41:22 +0800

    finish aggregate_test

--- slices/aggregate_test.go
+++ slices/aggregate_test.go
@@ -1,23 +1,197 @@
 package slices
 
-import "testing"
+import (
+	"errors"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
 func TestMax(t *testing.T) {
-
+	testCase := []struct {
+		name    string
+		slice   []int
+		want    int
+		wantErr error
+	}{
+		{
+			name:    "1",
+			slice:   []int{1, 2, 3, 4, 5},
+			want:    5,
+			wantErr: nil,
+		},
+		{
+			name:    "empty slice",
+			slice:   []int{},
+			want:    0,
+			wantErr: errors.New("slice is empty"),
+		},
+		{
+			name:    "3",
+			slice:   []int{999, 5, 46, 246, 15679, 154, 76649, 32, 45, 78, 66},
+			want:    76649,
+			wantErr: nil,
+		},
+	}
+	for _, testCase := range testCase {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := Max(testCase.slice)
+			assert.Equal(t, testCase.want, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, testCase.want, val)
+		})
+	}
 }
 
 func TestMin(t *testing.T) {
-
+	testCase := []struct {
+		name    string
+		slice   []int
+		want    int
+		wantErr error
+	}{
+		{
+			name:    "1",
+			slice:   []int{1, 2, 3, 4, 5},
+			want:    1,
+			wantErr: nil,
+		},
+		{
+			name:    "empty slice",
+			slice:   []int{},
+			want:    0,
+			wantErr: errors.New("slice is empty"),
+		},
+		{
+			name:    "3",
+			slice:   []int{999, 5, 46, 246, 15679, 154, 76649, 32, 45, 78, 66},
+			want:    5,
+			wantErr: nil,
+		},
+	}
+	for _, testCase := range testCase {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := Min(testCase.slice)
+			assert.Equal(t, testCase.want, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, testCase.want, val)
+		})
+	}
 }
 
 func TestSum(t *testing.T) {
-
+	testCase := []struct {
+		name    string
+		slice   []int
+		want    int
+		wantErr error
+	}{
+		{
+			name:    "1",
+			slice:   []int{1, 2, 3, 4, 5},
+			want:    15,
+			wantErr: nil,
+		},
+		{
+			name:    "empty slice",
+			slice:   []int{},
+			want:    0,
+			wantErr: errors.New("slice is empty"),
+		},
+		{
+			name:    "3",
+			slice:   []int{999, 5, 46, 246, 15679, 154, 76649, 32, 45, 78, 66},
+			want:    93999,
+			wantErr: nil,
+		},
+	}
+	for _, testCase := range testCase {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := Sum(testCase.slice)
+			assert.Equal(t, testCase.want, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, testCase.want, val)
+		})
+	}
 }
 
 func TestAvg(t *testing.T) {
-
+	testCase := []struct {
+		name    string
+		slice   []int
+		want    int
+		wantErr error
+	}{
+		{
+			name:    "1",
+			slice:   []int{1, 2, 3, 4, 5},
+			want:    3,
+			wantErr: nil,
+		},
+		{
+			name:    "empty slice",
+			slice:   []int{},
+			want:    0,
+			wantErr: errors.New("slice is empty"),
+		},
+		{
+			name:    "3",
+			slice:   []int{999, 5, 46, 246, 15679, 154, 76649, 32, 45, 78, 66},
+			want:    8545,
+			wantErr: nil,
+		},
+	}
+	for _, testCase := range testCase {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := Avg(testCase.slice)
+			assert.Equal(t, testCase.want, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, testCase.want, val)
+		})
+	}
 }
 
 func TestCount(t *testing.T) {
-
+	testCase := []struct {
+		name    string
+		slice   []int
+		want    int
+		wantErr error
+	}{
+		{
+			name:    "1",
+			slice:   []int{1, 2, 3, 4, 5, 5, 3, 2, 1, 1, 1},
+			want:    5,
+			wantErr: nil,
+		},
+		{
+			name:    "empty slice",
+			slice:   []int{},
+			want:    0,
+			wantErr: errors.New("slice is empty"),
+		},
+		{
+			name:    "3",
+			slice:   []int{999, 5, 46, 246, 15679, 154, 76649, 32, 45, 78, 66, 999, 246, 154, 154, 999, 999},
+			want:    11,
+			wantErr: nil,
+		},
+	}
+	for _, testCase := range testCase {
+		t.Run(testCase.name, func(t *testing.T) {
+			val, err := Count_Distinct(testCase.slice)
+			assert.Equal(t, testCase.want, err)
+			if err != nil {
+				return
+			}
+			assert.Equal(t, testCase.want, val)
+		})
+	}
 }
